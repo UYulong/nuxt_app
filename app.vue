2d429cd181f065e949aa13db081bda822fe36b08
UYulong <1274583083@qq.com>
2024-07-22 15:59:58 +0800

    ✨ feat(deps): 添加合配置naive-ui

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-  </div>
+  <NConfigProvider inline-theme-disabled>
+    <n-button type="primary">
+      Primary
+    </n-button>
+  </NConfigProvider>
 </template>
+
+<script setup>
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,28 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["nuxtjs-naive-ui"]
+  modules: ["nuxtjs-naive-ui"],
+  vite: {
+    plugins: [
+      AutoImport({
+        imports: [
+          {
+            'naive-ui': [
+              'useDialog',
+              'useMessage',
+              'useNotification',
+              'useLoadingBar'
+            ]
+          }
+        ]
+      }),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ]
+  }
 })
