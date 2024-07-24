caa9090d9001eb21233614dbd4248f4ba6d47d50
UYulong <1274583083@qq.com>
2024-07-24 10:37:00 +0800

    ✨ feat(视频): 视频直播页面开发

---
+++ components/Player/Audio.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="aplayer" />
+</template>
+<script setup>
+let ap = ref(null);
+
+const props = defineProps({
+  title: String,
+  cover: String,
+  url: String,
+});
+
+onMounted(() => {
+  ap.value = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [
+      {
+        name: props.title,
+        artist: 'Nuxt App',
+        url: props.url,
+        cover: props.cover,
+      },
+    ],
+  });
+});
+
+onBeforeUnmount(() => ap.value.destroy());
+</script>
---
+++ components/Player/Video.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="mse" />
+</template>
+
+<script setup>
+const props = defineProps({
+  url: String,
+});
+
+const player = ref(null);
+
+onMounted(() => {
+  player.value = new Player({
+    id: 'mse',
+    url: props.url,
+    width: null,
+    height: 450,
+    playsinline: true,
+  });
+});
+
+onBeforeUnmount(() => player.value.destroy());
+</script>
